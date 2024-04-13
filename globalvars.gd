18f76f2b668460e236e107d8d2a01b2522273fa7
Aureus <crazybluebird9@gmail.com>
2024-04-13 01:57:58 -0700

    Added cities screen. still nonfunctional

---
+++ globalvars.gd
@@ -0,0 +1,3 @@
+extends Node
+var cityname = "Burnaby"
+var apikey = ""
--- main.gd
+++ main.gd
@@ -4,7 +4,8 @@ var cities = load("res://cities.tscn")
 var history = load("res://history.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	add_child(current.instantiate())
+	move_child($Current, 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,24 +15,24 @@ func _process(delta):
 
 func _on_tab_container_tab_changed(tab):
 	if tab == 1:
-		if get_tree().root.get_child(0).has_node('History'):
+		if get_tree().root.get_child(1).has_node('History'):
 			$History.queue_free()
-		if get_tree().root.get_child(0).has_node('Cities'):
+		if get_tree().root.get_child(1).has_node('Cities'):
 			$Cities.queue_free()
 		add_child(current.instantiate())
 		move_child($Current, 0)
 	else:
 		if tab == 0:
-			if get_tree().root.get_child(0).has_node('Current'):
+			if get_tree().root.get_child(1).has_node('Current'):
 				$Current.queue_free()
-			if get_tree().root.get_child(0).has_node('History'):
+			if get_tree().root.get_child(1).has_node('History'):
 				$History.queue_free()
 			add_child(cities.instantiate())
 			move_child($Cities, 0)
 		else:
-			if get_tree().root.get_child(0).has_node('Cities'):
+			if get_tree().root.get_child(1).has_node('Cities'):
 				$Cities.queue_free()
-			if get_tree().root.get_child(0).has_node('Current'):
+			if get_tree().root.get_child(1).has_node('Current'):
 				$Current.queue_free()
 			add_child(history.instantiate())
 			move_child($History, 0)
