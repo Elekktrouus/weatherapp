c8566298fe4644c77147c399469d4d7288222955
Aureus <crazybluebird9@gmail.com>
2024-04-16 11:54:46 -0700

    made it work lol

--- current.gd
+++ current.gd
@@ -1,19 +1,23 @@
 extends Control
-
-
-# Called when the node enters the scene tree for the first time.
+@onready var currentdate = Time.get_datetime_dict_from_system()
+@onready var day = currentdate
 func _ready():
 	$CityName.text = (Globalvars.cityname)
 	getForecast(Globalvars.cityname, 7)
+	var timer = get_tree().create_timer(60)
+	while true:
+		await timer.timeout
+		getForecast(Globalvars.cityname, 7)
 func getForecast(cityname, days):
-	var requester = HTTPRequest.new()
-	add_child(requester)
-	requester.request_completed.connect(_on_request_completed)
-	requester.request(str("http://api.weatherapi.com/v1/forecast.json?key=", Globalvars.apikey, "&q=", cityname, "&days=", days, "&aqi=no&alerts=no"))
-
+		var requester = HTTPRequest.new()
+		add_child(requester)
+		requester.request_completed.connect(_on_request_completed)
+		requester.request(str("http://api.weatherapi.com/v1/forecast.json?key=", Globalvars.apikey, "&q=", "canada/",cityname, "&days=", days, "&aqi=no&alerts=no"))
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json)
-	
+	$CurrentTemp.text = (str(round(json["current"]["temp_c"]))+"°")
+	for i in 7:
+		$DayDisplay.get_child(i).text = (str(json["forecast"]["forecastday"][i]["date"]))
+		$TempDisplay.get_child(i).text = ((str(round(json["forecast"]["forecastday"][i]["day"]["mintemp_c"]))+"°")+" | "+(str(round(json["forecast"]["forecastday"][i]["day"]["maxtemp_c"]))+"°"))
 func _process(delta):
 	pass
