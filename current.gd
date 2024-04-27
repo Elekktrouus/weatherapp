0e3ef06c139f4b53fe1a60c91831fa8b83fb04cb
Aureus <crazybluebird9@gmail.com>
2024-04-26 23:43:01 -0700

    ACTUAL final commit. Added 3 day mode for people who have the free weather api plan and a few other implementations to fit the criteria for the apcsp assignment

--- current.gd
+++ current.gd
@@ -5,9 +5,9 @@ extends Control
 func _ready():
 	$CityName.text = (Globalvars.cityname)
 	getForecast(Globalvars.cityname, 7)
-
+	
 func getForecast(cityname, days):
-	if $CheckButton.button_pressed:
+	if $IsUpdate.button_pressed:
 		$UpdateIn.show()
 		var requester = HTTPRequest.new()
 		add_child(requester)
@@ -18,9 +18,18 @@ func getForecast(cityname, days):
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	$CurrentTemp.text = (str(round(json["current"]["temp_c"]))+"°")
-	for i in $TempDisplay.get_child_count():
-		$DayDisplay.get_child(i).text = (str(json["forecast"]["forecastday"][i]["date"]))
-		$TempDisplay.get_child(i).text = ((str(round(json["forecast"]["forecastday"][i]["day"]["mintemp_c"]))+"°")+" | "+(str(round(json["forecast"]["forecastday"][i]["day"]["maxtemp_c"]))+"°"))
+	if $IsThreeDay.button_pressed:
+		for i in $TempDisplay.get_child_count():
+			if i < 3:
+				$DayDisplay.get_child(i).text = (str(json["forecast"]["forecastday"][i]["date"]))
+				$TempDisplay.get_child(i).text = ((str(round(json["forecast"]["forecastday"][i]["day"]["mintemp_c"]))+"°")+" | "+(str(round(json["forecast"]["forecastday"][i]["day"]["maxtemp_c"]))+"°"))
+			else:
+				$DayDisplay.get_child(i).text = "Unavailable"
+				$TempDisplay.get_child(i).text = "Unavailable"
+	else:
+		for i in $TempDisplay.get_child_count():
+			$DayDisplay.get_child(i).text = (str(json["forecast"]["forecastday"][i]["date"]))
+			$TempDisplay.get_child(i).text = ((str(round(json["forecast"]["forecastday"][i]["day"]["mintemp_c"]))+"°")+" | "+(str(round(json["forecast"]["forecastday"][i]["day"]["maxtemp_c"]))+"°"))
 func _process(delta):
 	if (round($Timer.time_left)) != 1:
 		$UpdateIn.text = ("Update in "+str(round($Timer.time_left))+" seconds.")
